d8072ecae599eaf2c10cc71e0c12db210be5a2cb
dadalee51 <dadalee51@gmail.com>
2017-01-13 00:33:31 +1000

    initial setup

---
+++ grade-scores/grade-scores/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace grade_scores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
