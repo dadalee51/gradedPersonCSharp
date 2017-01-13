4b46c0c2b10318ec01b5b41d645d6ce8b5f2764b
dadalee51 <dadalee51@gmail.com>
2017-01-13 11:41:25 +1000

    unit test with code

--- grade-scores/UnitTestGradeScore/UnitTest1.cs
+++ grade-scores/UnitTestGradeScore/UnitTest1.cs
@@ -1,21 +1,73 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using grade_scores;
+using System.Collections.Generic;
 
 namespace UnitTestGradeScore
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        List<Person> lp, rp; 
+        [TestInitialize]
+        public void TestInitialize()
         {
-            Assert.AreEqual(2,2);
+            //this is unsorted order
+            rp = new List<Person>();
+            rp.Add(new Person("KING", "MADISON", 88));
+            rp.Add(new Person("SMITH", "ALLAN", 70));
+            rp.Add(new Person("SMITH", "FRANCIS", 85));
+            rp.Add(new Person("BUNDY", "TERESSA", 88));
+
+            //sorted order
+            lp = new List<Person>();
+            lp.Add(new Person("BUNDY", "TERESSA", 88));
+            lp.Add(new Person("KING", "MADISON", 88));
+            lp.Add(new Person("SMITH", "FRANCIS", 85));
+            lp.Add(new Person("SMITH", "ALLAN", 70));
+        }
+        
+        [TestMethod]
+        public void TestNoChangeOnOriginalList()
+        {
+            Person.sort(rp);
+            Assert.AreEqual(88, rp[0].grade);
+            Assert.AreEqual("KING", rp[0].lastName);
+            Assert.AreEqual("MADISON", rp[0].firstName);
+
+            Assert.AreEqual(70, rp[1].grade);
+            Assert.AreEqual("SMITH", rp[1].lastName);
+            Assert.AreEqual("ALLAN", rp[1].firstName);
+
+            Assert.AreEqual(85, rp[2].grade);
+            Assert.AreEqual("SMITH", rp[2].lastName);
+            Assert.AreEqual("FRANCIS", rp[2].firstName);
+
+            Assert.AreEqual(88, rp[3].grade);
+            Assert.AreEqual("BUNDY", rp[3].lastName);
+            Assert.AreEqual("TERESSA", rp[3].firstName);
+
         }
 
         [TestMethod]
-        public void TestMethod2()
+        public void TestSortCorrectness()
         {
-            Assert.AreEqual("Fellow".ToUpper(), "FELLOW".ToUpper());
+            List<Person> op = Person.sort(lp);
+            for(int i = 0; i < op.Count; i++)
+            {
+                Assert.AreEqual(op[i].grade, lp[i].grade);
+                Assert.AreEqual(op[i].lastName, lp[i].lastName);
+                Assert.AreEqual(op[i].firstName, lp[i].firstName);
+            }
         }
+
+        [TestMethod]
+              [ExpectedException(typeof(System.IO.FileNotFoundException),"file not found.")]
+        public void TestFileNotFoundError()
+        {
+            Person.Main(new string[1] { "nosuchfile.txt" });
+        }
+
+        
     }
 }
--- grade-scores/grade-scores/Program.cs
+++ grade-scores/grade-scores/Program.cs
@@ -1,16 +1,72 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
 
 namespace grade_scores
 {
-    class Program
+    public class Person
     {
-        static void Main(string[] args)
+        public string lastName;
+        public string firstName;
+        public int grade;
+        public Person(string ln, string fn, int gr)
         {
-            
+            lastName = ln;
+            firstName = fn;
+            grade = gr;
+        }
+
+        static public List<Person> sort(List<Person> list)
+        {
+            var outList = list.OrderByDescending(x => x.grade)
+                .ThenBy(x => x.lastName)
+                .ThenBy(x => x.firstName)
+                .ToList<Person>();
+            return outList;
+        }
+
+        public static void Main(string[] args)
+        {
+            //check argument,
+            if (!args.Any()) Trace.TraceError("Usage: grade-score input");
+            var path = args[0];
+            //check input file,
+            if (!File.Exists(path))
+            {
+                Trace.TraceError("input file not found:" + path);
+                throw new FileNotFoundException();
+            }
+            TextReader txread = File.OpenText(path);
+
+            List<Person> list = new List<Person>();
+            for (string line; (line = txread.ReadLine()) != null;)
+            {
+                string[] data = line.Split(',');
+                if (data.Length != 3) Trace.TraceError(String.Format("not enough fields in file input: {0} , prog exit.", line));
+                try
+                {
+                    int grade = Int32.Parse(data[2]);
+                    Person p = new Person(data[0].Trim(), data[1].Trim(), grade);
+                    list.Add(p);
+                }
+                catch (FormatException e)
+                {
+                    Trace.TraceError(String.Format("grade field input format error : {0} ;", data[2]) + e.Message);
+                }
+            }
+            //order by grade, lastName, then firstName:
+            StreamWriter fOut = new StreamWriter(@"names-graded.txt", false);
+            var newList = Person.sort(list);
+            foreach (var p in newList)
+            {
+                Console.WriteLine("{0}, {1}, {2}", p.lastName, p.firstName, p.grade);
+                fOut.WriteLine("{0}, {1}, {2}", p.lastName, p.firstName, p.grade);
+            }
+            fOut.Flush();
+            fOut.Close();
+            Console.WriteLine("Finished: created names-graded.txt ");
         }
     }
 }
